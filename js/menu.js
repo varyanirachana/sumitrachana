14389661b0f77ac3339afd904698b21ad1c44a48
Rachana Varyani <108489723+varyanirachana@users.noreply.github.com>
2022-07-02 12:34:37 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,13 @@
 function loginMessage() {
-
+ clear();
  document.getElementById("content")
   .innerHTML =
   '<b>Please log in to proceed</b>';
+}
+
+function clear() {
+
+ document.getElementById("content")
+  .innerHTML =
+  "";
 }
